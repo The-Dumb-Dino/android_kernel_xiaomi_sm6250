158339f85b4c42be718e34f2c99a580ef55d0d6b
Minchan Kim <minchan@google.com>
2019-03-11 11:39:02 +0900

    Revert "mm: provide speculative fault infrastructure"
    This reverts commit ce37f07c6b6374e48b2ea986359552dcf6d3daf5.
    
    Conflicts:
    	include/linux/mm.h
    
    Bug: 128240262
    Change-Id: I1060d8e43cebca1826a489fa3271279501c8647c
    Signed-off-by: Minchan Kim <minchan@google.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -315,7 +315,6 @@ extern pgprot_t protection_map[16];
  * @FAULT_FLAG_INSTRUCTION: The fault was during an instruction fetch.
  * @FAULT_FLAG_INTERRUPTIBLE: The fault can be interrupted by non-fatal signals.
  * @FAULT_FLAG_PREFAULT_OLD: Make faultaround ptes old.
- * @FAULT_FLAG_SPECULATIVE: Speculative fault, not holding mmap_sem.
  *
  * About @FAULT_FLAG_ALLOW_RETRY and @FAULT_FLAG_TRIED: we can specify
  * whether we would allow page faults to retry by specifying these two
@@ -347,7 +346,6 @@ extern pgprot_t protection_map[16];
 #define FAULT_FLAG_INSTRUCTION  		0x100
 #define FAULT_FLAG_INTERRUPTIBLE		0x200
 #define FAULT_FLAG_PREFAULT_OLD			0x400
-#define FAULT_FLAG_SPECULATIVE			0x800
 
 /*
  * The default fault flags that should be used by most of the
